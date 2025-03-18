eab34b037c81becabbbaeb2b214a095ae5bd54bb
Alberto <albertose_2704@hotmail.com>
2025-03-18 08:40:11 +0100

    lifecyrcle

--- examen_1/src/api/daily-menu/content-types/daily-menu/lifecyrcleCustom.ts
+++ examen_1/src/api/daily-menu/content-types/daily-menu/lifecyrcleCustom.ts
@@ -1,20 +1,14 @@
-/* module.exports = {
+/*module.exports = {
   async afterCreate(event) {
     const { result } = event;
-    const dishes = strapi.documents("api::plate.plate").findMany({
-      fields: ["nameOfDish", "priceOfDish"],
+    const plate = await strapi.documents("api::plate.plate").findMany({
+      fields: ["nameOfDish","priceOfDish"]
     });
-    const sumPrice = (dishes) => {
-      let total = 0;
-      for (let i = 0; i < dishes.length; i++) {
-        const price = parseFloat(dishes[i].priceOfDish) || 0;
-        total += price;
-      }
-      return total;
-    };
+    const totalPrice = plate.reduce((sum, plate) => sum + plate.priceOfDish,0);
     await strapi.documents("api::daily-menu.daily-menu").update({
-      data: { sumPrice: total},
+        documentId: result.id,
+        data: {sumPrice: totalPrice}
     });
   },
-}; 
+};
 */
