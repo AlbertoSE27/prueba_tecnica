e937a8d3ea1929f8c76ad716f4aa0a97eeb6241b
Alberto <albertose_2704@hotmail.com>
2025-03-19 08:40:47 +0100

    endpoints and sercice update

--- examen_1/endPoints/apis.ts
+++ examen_1/endPoints/apis.ts
@@ -1,25 +1,102 @@
 const { createCoreController } = require("@strapi/strapi").factories;
+
 module.exports = createCoreController(
-  "api:daily-menu.daily-menu",
+  "api::daily-menu.daily-menu",
   ({ strapi }) => ({
-    async dessertName(ctx) {
-      const { dessert } = ctx.request.query;
-      const dessertDate = await strapi.documents("api::dish.dish").findMany({
-        filters: { typeOfDish: "Dessert" },
-      });
-    },
-  })
-);
-module.exports = createCoreController(
-  "api:daily-menu.daily-menu",
-  ({ strapi }) => ({
-    async menuPrice(ctx) {
-      const { fixedPriceMenu } = ctx.request.query;
-      const menuDate = await strapi
-        .documents("api::daily-menu-daily-menu")
-        .findMany({
-          filters: { fixedPriceMenu: { $ite: 10, $gte: 20 } },
+    async getDessert(ctx) {
+      try {
+        const { dessert } = ctx.request.query;
+        if (!dessert) {
+          return ctx.badRequest("El postre no existe en el menú");
+        }
+        const dessertName = await strapi.documents("api::dish.dish").findMany({
+          filters: { typeOfDish: "Dessert" },
         });
+        if (!dessertName) {
+          return ctx.badRequest("El postre no existe en el menú");
+        }
+        return ctx.send(dessertName);
+      } catch (error) {
+        return ctx.throw(500, "Error interno del servidor");
+      }
     },
   })
 );
+
+module.exports = createCoreController(
+  "api::daily-menu.daily-menu",
+  ({ strapi }) => ({
+    async getMenuPrice(ctx) {
+      try {
+        const { fiexedPriceMenu } = ctx.request.query;
+        if (!fiexedPriceMenu) {
+          return ctx.badRequest("El menú no tiene precio");
+        }
+        const rangeMenuPrice = await strapi
+          .documents("api::daily-menu-daily-menu")
+          .findMany({
+            filters: { fixedPriceMenu: { $gte: 10, $ite: 20 } },
+          });
+        if (rangeMenuPrice < 10 || rangeMenuPrice > 20) {
+          return ctx.badRequest(
+            "No se encuentran menún con rando de precio entre 10 y 20"
+          );
+        }
+        return ctx.send(rangeMenuPrice);
+      } catch (error) {
+        return ctx.throw(500, "Error interno del servidor");
+      }
+    },
+  })
+);
+
+module.exports = createCoreController(
+  "api::daily-menu.daily-menu",
+  ({ strapi }) => ({
+    async getMenuWithoutAllergens(ctx) {
+      try {
+        const { menuDay, menuId } = ctx.request.query;
+        if (!menuDay || !menuId) {
+          return ctx.badRequest("No existe el menú");
+        }
+        const menuWithoutAllergens = await strapi
+          .documents("api:dish.dish")
+          .findMany({
+            filters: {
+              allergen: {
+                populate: {
+                  allergenName: { $notContains: ["gluten", "lactosa"] },
+                },
+              },
+            },
+          });
+        if (!menuWithoutAllergens) {
+          return ctx.badRequest("No existen menús sin gluten o sin lactosa");
+        }
+        return ctx.send(menuWithoutAllergens);
+      } catch (error) {
+        return ctx.throw(500, "Error interno del servidor");
+      }
+    },
+  })
+);
+
+module.exports = createCoreController("api::dish.dish", ({ strapi }) => ({
+  async getpopularDishes(ctx) {
+    try {
+      const { nameOfDish, dishId } = ctx.request.query;
+      if (!nameOfDish || !dishId) {
+        return ctx.badRequest("No existe el nombre ni el id del plato");
+      }
+      const poppularDishes = await strapi.documents("api::dish.dish").findMany({
+        filters: { nameOfDish: "*" },
+      });
+      if (!poppularDishes) {
+        return ctx.badRequest("No existen platos mas vendidos o populares");
+      }
+      return ctx.send(poppularDishes);
+    } catch (error) {
+      return ctx.throw(500, "Error interno del servidor");
+    }
+  },
+}));
--- examen_1/endPoints/sercice.ts
+++ examen_1/endPoints/sercice.ts
@@ -1,27 +1,35 @@
 import dailyMenu from "../src/api/daily-menu/controllers/daily-menu";
 const { createCoreService } = require("@strapi/strapi").factories;
-module.exports = createCoreService(
-  "api::daily-menu.daily-menu",
-  ({ strapi }) => ({
-    async calculateMenuPrice(menuDayId) {
-      try {
-        const dishDate = await strapi.documents("api::dish.dish").findMany({
-          fields: ["nameOfDish", "priceOfDish"],
-        });
-        const menuDate = await strapi
-          .documents("api::daily-menu.daily.menu")
-          .findMany({
-            fields: ["menuName", "fixedPriceMenu"],
-          });
-        const updateMenusPrice = fixedPriceMenu * 0.21;
-        await strapi.documents("api::daily-menu.daily-menu").update({
-          documentId: menuDayId,
-          data: { fixedPriceMenu: updateMenusPrice },
-        });
-      } catch (error) {
-        strapi.log.error("Error al calcular el precio del menu", error);
-        throw new error("Error al calcular el precio del menu");
+module.exports = createCoreService("api::dish.dish", ({ strapi }) => ({
+  async getPriceOfDish(priceOfDish) {
+    try {
+      const dishDate = await strapi.documents("api::dish.dish").findMany({
+        fields: ["nameOfDish", "priceOfDish"],
+      });
+      if (!dishDate) {
+        return { message: "No existe el plato o no tiene precio" };
       }
-    },
-  })
-);
+      const menuDate = await strapi
+        .documents("api::daily-menu.daily.menu")
+        .findMany({
+          fields: ["menuDay", "fixedPriceMenu"],
+        });
+      if (!menuDate) {
+        return { message: "No se encontró el menú o no tiene precio" };
+      }
+      const taxRate = 0.21;
+      const updateMenuPrice = menuDate[0].fixedPriceMenu * (1 + taxRate);
+      await strapi.documents("api::daily-menu.daily-menu").update({
+        documentId: menuDate.menuDay,
+        data: { fixedPriceMenu: updateMenuPrice },
+      });
+      return { dishDate, updateMenuPrice };
+    } catch (error) {
+      strapi.log.error(
+        "Error al calcular el precio del plato con impuestos",
+        error
+      );
+      throw new Error("Error interno del servidor");
+    }
+  },
+}));
